afae795b4cfa4c82f5324ea2123b12f7c8216165
seideljaron-wq <seideljaron@gmail.com>
2026-03-14 19:55:14 +0100

    Add ShopManager import to ConfirmGUI.java

--- src/main/java/dev/shardsystem/gui/ConfirmGUI.java
+++ src/main/java/dev/shardsystem/gui/ConfirmGUI.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import dev.shardsystem.managers.ShopManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
